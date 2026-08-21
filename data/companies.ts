852e2a2ec470d1cc0072f9051d3fc2bf8cc49b37
bajrang0704 <prabanjanreddy0704004@gmail.com>
2026-08-21 17:06:27 +0530

    logo update

--- data/companies.ts
+++ data/companies.ts
@@ -16,6 +16,6 @@ export const companies: Company[] = [
   { name: "Cognito Advisory", logo: "/logos/CognitoAdvisory.png", url: "https://cognitoadvisory.com/about/" },
   { name: "Cognito Energy", logo: "/logos/CognitoEnergy.png", url: "https://cognitoenergy.com/" },
   { name: "Houspire", logo: "/logos/Houspire.png", url: "https://houspire.ai/" },
-  { name: "Capabl", logo: "/logos/capabl.jpg", url: "https://www.capabl.in/" },
+  { name: "Capabl", logo: "/logos/capabl.avif", url: "https://www.capabl.in/" },
   { name: "Roastery", logo: "/logos/Roastery.webp", url: "https://roasterycoffee.co.in/" },
 ];
